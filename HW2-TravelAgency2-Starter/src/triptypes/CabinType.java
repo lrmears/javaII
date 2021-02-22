7b0a5246114fb5967da07a243545a8d32b3117d2
jacobsand <jacobsand@UITCS-EAB104-1.unomaha.edu>
2021-02-22 08:43:25 -0600

    re-ordered the enum types so they would be in the proper order

--- HW2-TravelAgency2-Starter/src/triptypes/CabinType.java
+++ HW2-TravelAgency2-Starter/src/triptypes/CabinType.java
@@ -14,23 +14,24 @@ public enum CabinType
 {
 
 	/**
-	 * An exterior room on board that has a small private balcony for guests.
-	 */
-	BALCONY,
-	
-	/**
 	 * An interior room on the ship with no window.
 	 */
 	INTERIOR,
-	
+
 	/**
 	 * An exterior room on the ship with a fixed window.
 	 */
 	OCEAN_VIEW,
 	
 	/**
+	 * An exterior room on board that has a small private balcony for guests.
+	 */
+	BALCONY,
+	
+	
+	/**
 	 * A luxury suite on the ship with a large balcony, sitting room, and separate bedroom.
 	 */
 	SUITE;
-
+	
 }
